85442da11899ebdb60fa78623e639005b18a3f97
micko.gabriel <micko.gabriel@10.53.52.8>
2014-12-06 14:02:51 +0100

    Request promise fix

--- js/request.js
+++ js/request.js
@@ -2,26 +2,25 @@ var apiURL = 'http://';
 var actionPart = '/omsz/alert';
 
 function getAlerts(location) {
-    var promise = new Promise();
-    
-    var xhr = new XMLHttpRequest(
-	{
-	    mozSystem: true,
+    var promise = new Promise(function(resolve, reject){
+	var xhr = new XMLHttpRequest(
+	    {
+		mozSystem: true,
+	    }
+	);
+	xhr.onreadystatechange = function() {
+	    if(xhr.readyState == 4 && xhr.status == 200) {
+		resolve(JSON.parse(xhr.responseText));
+	    }else{
+		reject(xhr.readyState);
+	    }
 	}
-    );
-    xhr.onreadystatechange = function() {
-	if(xhr.readyState == 4 && xhr.status == 200) {
-	    promise.resolve(JSON.parse(xhr.responseText));
-	}else{
-	    promise.reject(xhr.readyState);
-	}
-    }
-    
-    /**
-     * @TODO Validation of location. 
-     */
-    
-    xhr.open('GET', apiURL + actionPart + '?k=' + encodeURI(location) , true);
-    xhr.send();
+	
+	/**
+	* @TODO Validation of location. 
+	*/
+	xhr.open('GET', apiURL + actionPart + '?k=' + encodeURI(location) , true);
+	xhr.send();
+    });
     return promise;
 }
